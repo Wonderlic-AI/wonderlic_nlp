32536110621f5c54c5f5bb6cfb6e1049776d1bb4
Abdallah Aboelela <abdallah.aboelela@wonderlic.com>
2020-06-26 16:44:41 -0500

    Removing license from classifiers, auto-generated'

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GNU General Public License (GPLv3)',
     'Programming Language :: Python :: 3.7',
   ],
 )
